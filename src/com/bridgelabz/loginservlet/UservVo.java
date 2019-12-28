dfa531cdc91a95ecd2ad5aed964952ba4f4843fa
Rupesh <patilrupesh990@gmail.com>
2019-12-28 16:55:04 +0530

    [Rupesh Patil].added User bean

---
+++ src/com/bridgelabz/loginservlet/UservVo.java
@@ -0,0 +1,77 @@
+package com.bridgelabz.loginservlet;
+
+public class UservVo 
+{
+	private String firstName;
+	private String lastName;
+	private String userName;
+	private String address;
+	private String city;
+	private String state;
+	private int pincode;
+	private String Password;
+	private String gender;
+	private String contact;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public int getPincode() {
+		return pincode;
+	}
+	public void setPincode(int pincode) {
+		this.pincode = pincode;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getContact() {
+		return contact;
+	}
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+}
