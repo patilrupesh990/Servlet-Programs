41e79c740ede0731b0512f4eafd5468430cd282d
Rupesh <patilrupesh990@gmail.com>
2020-01-02 14:13:28 +0530

    [Rupesh Patil].updated LogoutServlet

---
+++ src/com/bridgelabz/loginservlet/Logout.java
@@ -0,0 +1,61 @@
+package com.bridgelabz.loginservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.omg.DynamicAny.DynAnyOperations;
+@SuppressWarnings("serial")
+
+/*************************************************************************************************************************
+ *  @author Rupeshp007
+ * date:28/12/2019
+ * @version 1.0
+ * 
+ * Its Servlet class Executes Bebofe Logout It will invalidate Session and redirect user to Login Page
+ * 
+ * url-pattern="logout"
+ * 
+ *******************************************************************************/
+@WebServlet("/logout")
+public class Logout extends HttpServlet
+{
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+	      PrintWriter out = response.getWriter();
+	      HttpSession session = request.getSession(true);
+	      // session.setAttribute("user", null);
+	      session.removeAttribute("address");
+	      session.removeAttribute("city");
+	      session.removeAttribute("state");
+	      session.removeAttribute("Contact");
+
+	      
+
+	      
+	      out.println("thank you!!, Your session was destroyed successfully &"
+	      		+ " Logout successfully!!");
+	      
+	            session.removeAttribute("name");
+	            session.invalidate();
+	            RequestDispatcher dispatcher=request.getRequestDispatcher("login.jsp");
+	            dispatcher.include(request, response);
+	     
+	}
+	
+	/**To destroy All connection resources*/
+
+	@Override
+	public void destroy()
+	{
+		UserDAO.closeConnection();
+	}
+}
