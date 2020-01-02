e59d7e572bac0022cc1eb4dbe93545bfd9fd3c08
Rupesh <patilrupesh990@gmail.com>
2020-01-02 14:15:28 +0530

    [Rupesh Patil].updated UserDAo

--- src/com/bridgelabz/loginservlet/UserDAO.java
+++ src/com/bridgelabz/loginservlet/UserDAO.java
@@ -5,13 +5,29 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+/***********************************************************************************************************
+ * @author Rupeshp007
+ * date:29/12/2019
+ * @version 1.0
+ * Purpose: Its and DAO layer class of Servlet login it contains database connection object and database operations related methods
+ * 			
+ * -insert()
+ * -update()
+ * -authentication()
+ * -checkEntry()
+ * -getConnection()
+ * 
+ **********************************************************************************************************/
 
 public class UserDAO 
 {
 	private static Connection connection;
 	private static PreparedStatement preparestatement;
 	private static ResultSet resultset;
-	
+
+	/*********************************************************************************
+	 *  Static block to load Driver and prepare Connection for Mysql database
+	 *******************************************************************************/
 	static
 	{
 		try 
@@ -24,25 +40,41 @@ public class UserDAO
 			e.printStackTrace();
 		}
 	}
-	
+	/*********************************************************************************
+	 *  private constructor to prevent Object creation
+	 *******************************************************************************/
 	private UserDAO() {
 	}
 	
+	/*********************************************************************************
+	 * To Provide Object of Connection class to calling methods.
+	 * 
+	 * @return Connection
+	 *******************************************************************************/
 	public static Connection getConnection()
 	{
 		return connection;
 	}
 	
+	
+	/***********************************************************************************************************************************************
+	 *  To Authenticate User Before LogedIn To Application it will check username and password entered by user is Valid or not
+	 *  
+	 *  
+	 *  @param String UserName,String password
+	 *  @return Resultset
+	 *******************************************************************************/
+
 	public static ResultSet Authentication(String username,String password)
 	{
-		
+
 		connection=UserDAO.getConnection();
-		
+
 		try {
-				preparestatement=connection.prepareStatement("select * from Register where UserName=? and password=?");
-				preparestatement.setString(1, username);
-				preparestatement.setString(2, password);
-				resultset=preparestatement.executeQuery();
+			preparestatement=connection.prepareStatement("select * from Register where UserName=? and password=?");
+			preparestatement.setString(1, username);
+			preparestatement.setString(2, password);
+			resultset=preparestatement.executeQuery();
 		} 
 		catch (SQLException e) 
 		{
@@ -50,13 +82,78 @@ public class UserDAO
 		}
 		return resultset;
 	}
+
 	
-	public static int insert(UservVo user)
+	/*************************************************************************************************************************
+	 * To check user is already registered or not it will check user by his Email and contact for Authenticate.
+	 *  
+	 *  @param UserDTO user
+	 *  @return boolean
+	 *******************************************************************************/
+	public static boolean checkEntry(UserDTO user)
+	{
+		connection=UserDAO.getConnection();
+		try{
+			preparestatement=connection.prepareStatement("select * from Register");
+
+			ResultSet resultset=preparestatement.executeQuery();
+			while(resultset.next())
+			{
+				if(resultset.getString(11).equals(user.getEmail()));
+				{
+					System.out.println(resultset.getString(11)+"database");
+
+					System.out.println(user.getEmail());
+					System.out.println("hello");
+					return false;
+				}
+			}
+			
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return true;
+	}
+	
+	
+	/*************************************************************************************************************************
+	 *  To update the Entry in database its use in Forgot Password of user to reset password  
+	 *  @param UserDTO user
+	 *  @return int
+	 *******************************************************************************/
+	
+	public static int update(UserDTO user)
 	{
 		connection=UserDAO.getConnection();
 		int result=0;
 		try{
-			preparestatement=connection.prepareStatement("insert into Register values(?,?,?,?,?,?,?,?,?,?)");
+			preparestatement=connection.prepareStatement("update Register set password=? where  contact=?");
+			preparestatement.setString(1,user.getPassword());
+			preparestatement.setString(2,user.getContact());
+			result=preparestatement.executeUpdate();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+
+	
+	/*************************************************************************************************************************
+	 * insert data into user data Table  its used to registration form of user.
+	 *  
+	 *  @param UserDTO user
+	 *  @return int
+	 *******************************************************************************/
+	public static int insert(UserDTO user)
+	{
+		connection=UserDAO.getConnection();
+		int result=0;
+		try{
+			preparestatement=connection.prepareStatement("insert into Register values(?,?,?,?,?,?,?,?,?,?,?)");
 			preparestatement.setString(1,user.getFirstName());
 			preparestatement.setString(2,user.getLastName());
 			preparestatement.setString(3, user.getUserName());
@@ -67,13 +164,29 @@ public class UserDAO
 			preparestatement.setString(8, user.getPassword());
 			preparestatement.setString(9, user.getGender());
 			preparestatement.setString(10, user.getContact());
-			 result=preparestatement.executeUpdate();
+			preparestatement.setString(11, user.getEmail());
+			result=preparestatement.executeUpdate();
+			
+			return result;
+
 		}
 		catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+		System.out.println(result);
 		return result;
 	}
 	
+	public static void closeConnection()
+	{
+		try {
+			connection.close();
+			preparestatement.close();
+			resultset.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 }
